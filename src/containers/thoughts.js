6fd35bd0eb92529a81119adb6cea61f81c31c56c
Stuart Quin <stuart.quin@gmail.com>
2016-02-14 09:15:26 +0000

    Tag popover

--- src/containers/thoughts.js
+++ src/containers/thoughts.js
@@ -6,13 +6,14 @@ import { connect } from 'react-redux';
 
 var {
   ListView,
+  StyleSheet,
   Text,
   TextInput,
+  TouchableHighlight,
   View
 } = React;
 
 const MaterialButton = MK.MKButton.button().build();
-
 
 const ThoughtList = ({
   thoughts
@@ -25,53 +26,131 @@ const ThoughtList = ({
   );
 };
 
-const AddThought = ({
-  onAdd
+const popOverStyles = StyleSheet.create({
+  contaner: {
+    position: 'absolute'
+  }
+});
+
+const TagPopOver = ({
+  tags,
+  onTagSelected
 }) => {
-  let currentValue;
   return (
-    <View>
-      <TextInput
-        onChangeText={(text) => {
-          currentValue = text
-        }}
-      />
-      <MaterialButton
-        onPress={() => {
-          onAdd(currentValue);
-          currentValue = '';
-        }}>
-        <Text>Add</Text>
-      </MaterialButton>
-    </View>
+    <ListView
+      dataSource={tags}
+      horizontal={true}
+      renderRow={tag => <Tag tag={tag} onTagClick={onTagSelected} />}
+    />
   );
 };
 
+const tagStyles = StyleSheet.create({
+  container: {
+    marginRight: 10,
+    backgroundColor: '#3FB0AC',
+    padding: 10
+  },
+
+  text: {
+    color: '#FFFFFF'
+  }
+});
+
+const Tag = ({
+  tag,
+  onTagClick
+}) => {
+  return (
+    <MaterialButton
+    onPress={() => {
+      onTagClick(tag)
+    }}>
+      <Text text={tagStyles.text}>{tag}</Text>
+    </MaterialButton>
+  );
+};
+
+const getTagPrefix = (text) => {
+  const chunk = text.split(' ').reverse()[0];
+  if (chunk.startsWith('#')) {
+    return chunk.replace(/^#+/, "");
+  }
+  return null;
+};
+
+class AddThought extends React.Component {
+  render() {
+    const onAdd = this.props.onAdd;
+    const suggestedTags = this.props.suggestedTags;
+    const onSuggestTags = this.props.onSuggestTags;
+
+    let tagPrefix = null;
+
+    return (
+      <View>
+        <TextInput
+          ref={component => this._textInput = component}
+          onChangeText={(text) => {
+            this.currentValue = text;
+            this.tagPrefix = getTagPrefix(this.currentValue);
+            onSuggestTags(this.tagPrefix);
+          }}
+        />
+        <TagPopOver 
+          tags={suggestedTags}
+          onTagSelected={(tag) => {
+            const reg = new RegExp(`#${this.tagPrefix}(\\s|$)`);
+            this.currentValue = this.currentValue.replace(reg, `#${tag} `);
+            this._textInput.setNativeProps({
+              text: this.currentValue
+            });
+            this.forceUpdate();
+          }}/>
+        <MaterialButton
+          onPress={() => {
+            onAdd(this.currentValue);
+            onSuggestTags(null);
+            this._textInput.setNativeProps({text: ''});
+          }}>
+          <Text>Add</Text>
+        </MaterialButton>
+      </View>
+    );
+  };
+}
 
 class ThoughtsContainer extends React.Component {
   constructor(props) {
     super(props);
-
     this.dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
     });
   }
 
   getThoughtsDataSource(thoughts) {
-    return this.dataSource.cloneWithRows(thoughts);
+    return 
   }
 
   render() {
-    const thoughts = this.props.state.thoughts;
-
+    const thoughts = this.props.state.get('thoughts').toJS();
+    const suggestedTags = this.props.state.get('suggestedTags').toJS();
     return (
       <View style={{marginTop: 70}}>
         <ThoughtList
-          thoughts={this.getThoughtsDataSource(thoughts)} />
+          thoughts={this.dataSource.cloneWithRows(thoughts)} />
         <AddThought
+          suggestedTags={this.dataSource.cloneWithRows(suggestedTags)}
           onAdd={(value) => {
             this.props.dispatch({
               type: 'ADD_THOUGHT',
+              value
+            });
+          }}
+          onSuggestTags={(value) => {
+            console.log('onSuggestTags', value);
+            this.props.dispatch({
+              type: 'SUGGEST_TAGS',
               value
             });
           }}
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,24 @@
 'use strict';
+import {Map, List} from 'immutable';
+
+const fetchTags = (prefix, tags) => {
+  if (prefix) {
+    return tags.filter(t => t.startsWith(prefix)); 
+  }
+  return List();
+};
 
 export default function(state = Map(), action) {
   switch(action.type) {
     case 'ADD_THOUGHT':
-      return {
-        thoughts: state.thoughts.concat([action.value])
-      };
+      return state.set('thoughts', state.get('thoughts').concat([action.value]));
+    case 'SUGGEST_TAGS':
+      return state.set('suggestedTags', fetchTags(action.value, state.get('existingTags')));
   }
 
-  return  {
-    thoughts: []
-  };
+  return Map({
+    thoughts: List(),
+    existingTags: List(['hello', 'help', 'contact', 'note', 'cool', 'reminder']),
+    suggestedTags: List(),
+  });
 }
